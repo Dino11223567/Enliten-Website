25122a393126ea2815f09f801419d7d27879d814
Dino <henry11223567@gmail.com>
2018-02-13 20:39:31 -0600

    Update test.js

--- static/scripts/test.js
+++ static/scripts/test.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
-    $("#sample_button").bind("click", function () {
+    //display question
+    //check answer function
+    $("#next_question").bind("click", function () {
+        var question_original = document.getElementById("question").innerHTML;
+        var question_words = question_original.split(" ");
+        var timed_question = question_words[0];
+        for (i = 1; i < question_words.length; i++) {
+            var place_holder = " " + question_words[i];
+            timed_question += place_holder;
+            console.log(timed_question);
+            document.getElementById("question").innerHTML = timed_question;
+        }
+        return false
+    });
+    
+    $("#submit_answer").bind("click", function () {
         $.getJSON("/background_process", {
-            response: $("input[name='response'").val(),
+            response: $("input[name='response']").val(),
         },
             function (data) {
                 $("#result").text(data.result);
