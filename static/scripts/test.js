93e770eb9eb421fe92dcdb8e504a2130b3a79ad6
Dino <henry11223567@gmail.com>
2018-02-25 20:32:21 -0600

    Update test.js

--- static/scripts/test.js
+++ static/scripts/test.js
@@ -1,26 +1,55 @@
-$(document).ready(function () {
-    //display question
-    //check answer function
-    $("#next_question").bind("click", function () {
-        var question_original = document.getElementById("question").innerHTML;
-        var question_words = question_original.split(" ");
-        var timed_question = question_words[0];
-        for (i = 1; i < question_words.length; i++) {
-            var place_holder = " " + question_words[i];
-            timed_question += place_holder;
-            console.log(timed_question);
-            document.getElementById("question").innerHTML = timed_question;
+function test(question_string) {
+    var question_original = question_string;
+    var question_words = question_original.split(" ");
+    console.log(question_words.length);
+    var timed_question = question_words[0];
+    i = 0;
+    var displayer = setInterval(function () {
+        document.getElementById("question").innerHTML = timed_question;
+        i += 1;
+        var place_holder = " " + question_words[i];
+        timed_question += place_holder;
+        if (i == question_words.length) {
+            clearInterval(displayer);
         }
+    }, 125);
+       
+}
+
+
+
+$(document).ready(function () {
+    //next question
+    $("#next_question").bind("click", function () {
+        $.getJSON("/background_process", {
+            type: "Next"
+        },
+            function (data) {
+                test(data.result);
+            });
+        
         return false
     });
-    
+
+
+    //submit answeer
     $("#submit_answer").bind("click", function () {
         $.getJSON("/background_process", {
-            response: $("input[name='response']").val(),
+            type: "Answer",
+            response: $("input[name='response']").val()
         },
             function (data) {
                 $("#result").text(data.result);
             });
         return false
     });
+
+
+    //Test
+    $("#buzz").bind("click", function () {
+        clearInterval(displayer);
+        return false
+    });
+
+
 });
