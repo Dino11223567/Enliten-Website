473faa0c364c4113d23efb2111c4d9be168caed9
Dino <henry11223567@gmail.com>
2018-02-25 20:33:50 -0600

    Update App.py

--- App.py
+++ App.py
@@ -1,25 +1,28 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from flask_bootstrap import Bootstrap
-import time, json,urllib3, random
+import time, json,urllib3, random, sqlite3
 from random import randint
+import Enliten_Functions
 
-#Variables and Lists
-database= []
-
-global question_info
-global answer
-question_info={}
-answer=""
 
 #Functions
+Q = Enliten_Functions.Q_Tasks()
 
-with open("database/test.json") as json_data:    
-    for line in json_data:
-        database.append(json.loads(line))
-        
+
+Q.fetch()
+string =Q.Answer_str()
+print(string)
+print(string.split())
+x= input("A: ")
+if x in string.split():
+    print("Correct!")
+else:
+    print("Try Again")
+
+
+
 
 #Python Flask 
-
 
 
 app = Flask(__name__)
@@ -31,28 +34,16 @@ app.secret_key = "howsitgoingbro1234508234129458751239487"
 @app.route("/", methods=['GET'])
 def index():
 
-    question = "This is where a question will go "
-    words = question.split()
-    question_words= []
-    i = 0
-    string = ""
-    while (i< len(words)):
-        question_words.append(words[i])
-        string = " ".join(question_words)
-        i += 1
-    return render_template("home.html",TestQuestion=question_words)
+    return render_template("home.html")
 
 
 
 @app.route("/Question", methods=['GET'])
 def Question():
-    index = random.randint(0,len(database)-1)
-    question_info = database[index]
-    answer=question_info['answer']    
-    return render_template("question.html",question=question_info['question'])
-   
+    return render_template("question.html")
+  
 @app.route("/Login", methods=['GET'])
-def Login():
+def Login():    
     return("Currently Under Construction")
 
 @app.route("/Practice", methods=['GET'])
@@ -63,12 +54,26 @@ def Practice():
 def Profile():
     return("Currently Under Construction")
 
-@app.route("/background_process", methods=['GET'])
-def background_process():
-       ans = request.args.get('response')
-       print(ans)
-       return jsonify(result=ans)
 
+@app.route("/background_process", methods=['GET','POST'])
+def background_process():
+
+    response = request.args.get('response')
+    type = request.args.get('type')
+
+    if type == "Next":
+        Q.fetch()
+
+        return jsonify(result= Q.Question_str())
+        
+    elif type == "Answer":
+        
+        if response in Q.Answer_str.split():
+
+            return jsonify(result = "WOW DID YOU CHEAT?")
+
+        else:
+            return jsonify(result = Q.Answer_str())
         
 
 
